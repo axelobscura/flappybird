e7c3f456b9f9bc2d8c6ec5a81c83651eec87a6e8
Axel Obscura <axosar@gmail.com>
2020-03-08 18:16:25 -0600

    Canvas al 100 de ancho

--- src/Game.js
+++ src/Game.js
@@ -12,6 +12,8 @@ class Game extends Component {
     }
 draw = () => {
         const ctx = this.refs.canvas.getContext("2d");
+        ctx.canvas.width  = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
         ctx.fillStyle = "green";
         ctx.fillRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);           
         ctx.beginPath();
